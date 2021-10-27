4873cd06ffe96456d970e324db61fdd5518fe78c
TheKuznecoff <djontravolta888@gmail.com>
2021-10-27 16:05:33 +0300

    feat: add weather widget

--- js/script.js
+++ js/script.js
@@ -121,3 +121,31 @@ function getSliderPrev() {
    }
    setBg();
 }
+
+//Виджет погоды
+const weatherIcon = document.querySelector('.weather-icon');
+const apiKey = 'a81f75386803d2f30643592d53297c1c';
+const temperature = document.querySelector('.temperature');
+const weatherDescription = document.querySelector('.weather-description');
+const city = document.querySelector('.city');
+
+async function getWeather() {
+   const url = `https://api.openweathermap.org/data/2.5/weather?q=Волгоград&lang=ru&appid=08f2a575dda978b9c539199e54df03b0&units=metric`;
+   const res = await fetch(url);
+   const data = await res.json();
+ 
+   weatherIcon.classList.add(`owf-${data.weather[0].id}`);
+   temperature.textContent = `${Math.round(data.main.temp)}°C`;
+   weatherDescription.textContent = data.weather[0].description;
+ }
+ 
+
+ function setCity(event) {
+   if (event.code === 'Enter') {
+     getWeather();
+     city.blur();
+   }
+ }
+
+document.addEventListener('DOMContentLoaded', getWeather);
+city.addEventListener('keypress', setCity);
